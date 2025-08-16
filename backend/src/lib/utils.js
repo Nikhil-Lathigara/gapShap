a136dbb1469bc3225b097aac9ef364fd8a0c75b6
Nikhil Soni <85342986+Nikhilsoni-ux@users.noreply.github.com>
2025-08-16 16:34:28 +0530

    token fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,16 @@
-import jwt from "jsonwebtoken"
-export const generateToken=(userId,res)=>{
-const token = jwt.sign({userId},process.env.JWT_SECRET,{
-    expiresIn:"7d"
-})
-res.cookie("jwt",token,{
-    maxAge:7*24*60*60*1000, // miliseconds
-    httpOnly:true,// prevent XSS attacks cross-site scripting attacks
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !=="development"
+import jwt from "jsonwebtoken";
 
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-})
-return token;
-}
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true, // prevent XSS
+    secure: process.env.NODE_ENV !== "development", // cookie only over HTTPS
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none", // CORS friendly
+  });
+
+  return token;
+};
