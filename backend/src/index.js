c7338bc470b706a86ca18e7548a4a679c6e69a67
Nikhil Soni <85342986+Nikhilsoni-ux@users.noreply.github.com>
2025-08-16 16:06:06 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["*","https://gap-shap-eight.vercel.app","http://localhost:5174"],
     credentials: true,
   })
 );
